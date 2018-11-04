793545ee614763a2846d43274c66522aace540ed
utkarshm <utkarshm@umich.edu>
2018-11-04 11:58:26 -0500

    Added the python files

---
+++ test_rpn.py
@@ -0,0 +1,22 @@
+import unittest
+import rpn
+class TestBasics(unittest.TestCase):
+    def test_add(self):
+        result = rpn.calculate('1 1 +')
+        self.assertEqual(2, result)
+
+    def test_sub(self):
+        result = rpn.calculate('4 3 -')
+        self.assertEqual(1, result)
+
+    def test_multiply(self):
+        result = rpn.calculate('4 3 *')
+        self.assertEqual(12, result)
+
+    def test_divide(self):
+        result = rpn.calculate('6 3 /')
+        self.assertEqual(2, result)
+
+    def test_toomany(self):
+    	with self.assertRaises(ValueError):
+    		result = rpn.calculate('1 2 3 +')
